0eef4969805e95ddc6540eb0915c5d8f41b7414c
Satoshi Kobayashi <koba@yk.rim.or.jp>
2025-05-05 09:08:57 +0800

    ゲームの開始処理を追加

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,17 @@
  *  Released under the MIT license
  *  https://github.com/kobalab/NyankoSweeper/blob/master/LICENSE
  */
+
+const $ = require('jquery');
+
+const Game  = require('./game');
+const Board = require('./board');
+
+$(function(){
+    let game  = new Game(20, 12, 24);
+    let board = new Board($('#board'), game);
+    board.start();
+
+    $('#loading').hide();
+    $('#board').fadeIn();
+});
